14451d7c07d5cf0a00dc1d8b4e7e2dd707fa5490
Porter <peapodpuffer@gmail.com>
2024-06-19 22:10:46 +0700

    Selected a product from the webpage

--- src/main/scala/SequentialScraper.scala
+++ src/main/scala/SequentialScraper.scala
@@ -7,16 +7,28 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 
 
 object SequentialScraper {
+
+  case class Product(name: String, url: String, image: String, price: String)
+
   val browser = JsoupBrowser()
   val doc = browser.get("https://www.scrapingcourse.com/ecommerce/")
   val html = doc.toHtml
 
-  println(html)
+  // get the first HTML product on the page
+  val htmlProductElement = doc >> element("li.product")
+
+  // extract the desired data from it
+  val name = htmlProductElement >> text("h2")
+  val url = htmlProductElement >> element("a") >> attr("href")
+  val image = htmlProductElement >> element("img") >> attr("src")
+  val price = htmlProductElement >> text("span")
+
+
+  println(name + " " + url + " " + image + " " + price)
 }
 
 @main
 def main(): Unit = {
   println("hello world")
   val scraper = SequentialScraper
-  println(scraper)
 }
