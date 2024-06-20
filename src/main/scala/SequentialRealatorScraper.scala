69df9aa1c5377a5967e5534e1e3fc26a95ed0157
Porter <peapodpuffer@gmail.com>
2024-06-20 21:24:43 +0700

    Got one title off a real site

--- src/main/scala/SequentialRealatorScraper.scala
+++ src/main/scala/SequentialRealatorScraper.scala
@@ -6,19 +6,21 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Parse.*
 
 object SequentialRealatorScraper extends App{
   val browser = JsoupBrowser()
-  val doc = browser.get("https://www.lazada.co.th/shop-mobiles/")
+//  val doc = browser.get("https://www.lazada.co.th/shop-mobiles/")
+  val doc = browser.get("https://frbbq.com/menu/")
 
   // Use proper CSS selector to find the element
-  val htmlListingElement = doc >> element("Bm3ON")
+  val htmlListingElement = doc >> element("h1")
 
-//  val name = htmlListingElement >> text("span")
+  val name = htmlListingElement >> text("h1")
 
-  val title = htmlListingElement >> text(".RfADt a")
-  val price = htmlListingElement >> text(".aBrP0 .ooOxS")
-
-  println(s"Title: $title")
-  println(s"Price: $price")
-  println("-------------------------")
+  println(name)
+//  val title = htmlListingElement >> text(".RfADt a")
+//  val price = htmlListingElement >> text(".aBrP0 .ooOxS")
+//
+//  println(s"Title: $title")
+//  println(s"Price: $price")
+//  println("-------------------------")
 
   // Print the HTML of the selected element
 //  println(name)
