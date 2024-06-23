68c94ea3ad800f1b2df72adb6895cbc206fa942c
Porter <peapodpuffer@gmail.com>
2024-06-23 23:15:50 +0700

    Got two websites scraping as futures
    This means that connections and scraping is done in parallel. Right now it's on a smaller scale, I could time it to see if there's speedup now. Likely need to increase data set to see noticeably speedup.

---
+++ src/main/scala/LargeScraper.scala
@@ -0,0 +1,3 @@
+object LargeScraper extends App{
+  
+}
--- src/main/scala/attemptingParallelism.scala
+++ src/main/scala/attemptingParallelism.scala
@@ -1,24 +1,26 @@
+import SequentialRealatorScraper.{beerList, beerListElements}
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+import net.ruippeixotog.scalascraper.dsl.DSL.*
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract.*
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse.*
 import net.ruippeixotog.scalascraper.model.Document
 
 import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.duration._
+import scala.concurrent.duration.*
 import scala.util.{Failure, Success}
 
 object attemptingParallelism extends App {
   val browser = JsoupBrowser()
   implicit val ec: ExecutionContext = ExecutionContext.global
-//  val docOne = browser.get("https://en.wikipedia.org/wiki/Main_Page")
-//  val htmlListingElement = docOne >> element("h1")
-//  println(htmlListingElement)
-//
-//  val name = htmlListingElement >> text("h1")
-//  println(name)
-//  println("WTF")
+  //  val docOne = browser.get("https://en.wikipedia.org/wiki/Main_Page")
+  //  val htmlListingElement = docOne >> element("h1")
+  //  println(htmlListingElement)
+  //
+  //  val name = htmlListingElement >> text("h1")
+  //  println(name)
+  //  println("WTF")
 
+  //function to turn page connectoin into a future
   def fetchPage(url: String): Future[Document] = Future {
     try {
       val newBrowser = JsoupBrowser()
@@ -44,16 +46,26 @@ object attemptingParallelism extends App {
   val menuFuture = fetchPage("https://frbbq.com/menu/")
   val beerListFuture = fetchPage("https://frbbq.com/beer-list/")
 
-  val htmlListingElementFuture = menuFuture.map(menu => menu >> element("h1")) 
-  
-  val nameFuture = htmlListingElementFuture.map(htmlListingElement => htmlListingElement >> text("h1")) 
-  nameFuture.onComplete{
+  //Scraping website 1
+  val htmlListingElementFuture = menuFuture.map(menu => menu >> element("h1"))
+  val nameFuture = htmlListingElementFuture.map(htmlListingElement => htmlListingElement >> text("h1"))
+  nameFuture.onComplete {
     case Success(doc) =>
       println(nameFuture)
     case Failure(_) => ???
   }
-  
 
+  //scraping website 2
+  val beerListElementsFuture = beerListFuture.map(beerList => beerList >> elements("h5"))
+
+  beerListElementsFuture.map(beerListElements => beerListElements.foreach { priceElement =>
+    val price = priceElement.text
+    println(s"Price: $price")
+  })
+
+  Await.result(beerListElementsFuture, 30.seconds)
+  Await.result(nameFuture, 30.seconds)
+  
   menuFuture.onComplete {
     case Success(doc) =>
       logDocument(doc)
@@ -67,26 +79,26 @@ object attemptingParallelism extends App {
     case Failure(exception) =>
       logError("https://en.wikipedia.org/wiki/Main_Page (beer list)", exception)
   }
-
-  // Combining futures to ensure both pages are fetched before proceeding
-  val combinedFuture = for {
-    menuDoc <- menuFuture
-    beerListDoc <- beerListFuture
-  } yield (menuDoc, beerListDoc)
-
-  combinedFuture.onComplete {
-    case Success((menuDoc, beerListDoc)) =>
-      println("Both documents fetched successfully.")
-    case Failure(exception) =>
-      println(s"Failed to fetch one or both documents: ${exception.getMessage}")
-  }
-
-  // Increase the timeout duration to 60 seconds for testing
-  try {
-    Await.result(combinedFuture, 60.seconds)
-    println("Processing completed within timeout.")
-  } catch {
-    case e: Exception =>
-      println(s"Processing timed out. Error: ${e.getMessage}")
-  }
 }
+  // Combining futures to ensure both pages are fetched before proceeding
+//  val combinedFuture = for {
+//    menuDoc <- menuFuture
+//    beerListDoc <- beerListFuture
+//  } yield (menuDoc, beerListDoc)
+
+//  combinedFuture.onComplete {
+//    case Success((menuDoc, beerListDoc)) =>
+//      println("Both documents fetched successfully.")
+//    case Failure(exception) =>
+//      println(s"Failed to fetch one or both documents: ${exception.getMessage}")
+//  }
+
+//  // Increase the timeout duration to 60 seconds for testing
+//  try {
+//    Await.result(combinedFuture, 60.seconds)
+//    println("Processing completed within timeout.")
+//  } catch {
+//    case e: Exception =>
+//      println(s"Processing timed out. Error: ${e.getMessage}")
+//  }
+//}
