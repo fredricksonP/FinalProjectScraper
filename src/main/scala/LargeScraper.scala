7cd4ac604666e5594058e8abcbd5f5a0a8e9d297
Porter <peapodpuffer@gmail.com>
2024-06-24 21:34:52 +0700

    Think I found a pretty good site to scrape
    One drawback is that it requires me to "Click" show more results to get more data. I've looked into how to do this and selenium is required for dynamic page generation and clicks. I'm downloading the proper software now.

--- src/main/scala/LargeScraper.scala
+++ src/main/scala/LargeScraper.scala
@@ -1,3 +1,4 @@
+import SequentialScraper.{doc, htmlProductElements}
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.*
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract.*
@@ -10,7 +11,14 @@ import net.ruippeixotog.scalascraper.model.Document
 object LargeScraper extends App{
   //First connect to a realty website
   //TODO: Find a website for analysis
+//  https://www.realtor.com/realestateandhomes-search/Colorado-Springs_CO
+  val browser = JsoupBrowser()
+//  val doc = browser.get("https://www.thailand-property.com/properties-for-sale/nakhon-pathom/phutthamonthon/salaya?exact_bed=false")
+  val doc = browser.get("https://www.fivestars-thailand.com/en/sale/bangkok")
   //TODO: Test a new web connection with jsoup
+  val htmlHouseElements = doc >> elementList("div.property-block")
+  println(htmlHouseElements)
+  println("There are " + htmlHouseElements.length + " in in the htmlHouseElements")
   //Next, pull elements
   //TODO: pull elements and print the HTML to make sure proper html is parsed
   //Process elements
