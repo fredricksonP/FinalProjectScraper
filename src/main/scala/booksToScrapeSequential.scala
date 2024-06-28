51ed28b750f754cd48bee45ece2a003b1adcfb78
Porter <peapodpuffer@gmail.com>
2024-06-28 23:27:21 +0700

    Can't get anything to work tonight

--- src/main/scala/booksToScrapeSequential.scala
+++ src/main/scala/booksToScrapeSequential.scala
@@ -14,18 +14,42 @@ import java.util.concurrent.atomic.AtomicLong
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.io.Source
-import scala.math._
+
+import java.util.concurrent.ConcurrentLinkedQueue
+
+class AtomicallyLockedVec {
+  private var setOfVisited: Vector[Double] = Vector()
+
+  def addElement(newElement: Double): Unit = this.synchronized {
+    setOfVisited = setOfVisited :+ newElement // Update the set with the new link
+  }
+
+  def get: Vector[Double] = this.synchronized {
+    setOfVisited
+  }
 
 object booksToScrapeSequential extends App{
   //open the csv file of all links to books to scrape
   // Specify the path to your file
-  var highestBookPrice = AtomicDouble(0.0) //The atomic double used for highest price
+//  var highestBookPrice = AtomicDouble(0.0) //The atomic double used for highest price
 
   implicit val ec: ExecutionContext = ExecutionContext.global
   val filePath = "src/main/scala/bookslinks.csv"
 
   // Open the file
   val bufferedSource = Source.fromFile(filePath)
+
+//  val concurrentQueue = new ConcurrentLinkedQueue[Double]()
+
+  val pricesVec = new AtomicallyLockedVec()
+//  def contains(elementToCheck: Double): Boolean = this.synchronized {
+//    setOfVisited.contains(elementToCheck)
+//  }
+
+  def size: Int = this.synchronized {
+    setOfVisited.size
+  }
+}
 
   def fetchPage(url: String): Future[Document] = Future {
     try {
@@ -38,15 +62,16 @@ object booksToScrapeSequential extends App{
       val bookPriceParsed = bookPrice.text
       val bookPriceDouble = bookPriceParsed.drop(1).toDouble
 
-//      if (bookPriceDouble > highestBookPrice.get()) {
-//        print("yo")
-//      }
-//      highestBookPrice = max(book)
+      //Keep track of all the prices in a concurrent linked queue
+//      concurrentQueue.add(bookPriceDouble)
+
+      pricesVec.addElement(bookPriceParsed)
+
       println("Title: " + bookTitle)
       println("Price: " + bookPriceParsed)
-
-      println(s"Fetched page successfully: $url")
-//      doc
+//
+//      println(s"Fetched page successfully: $url")
+      doc
     } catch {
       case e: Exception =>
         println(s"Failed to fetch page: $url. Error: ${e.getMessage}")
@@ -69,6 +94,11 @@ object booksToScrapeSequential extends App{
   // Calculate the duration
   val durationInSeconds = (endTime - startTime) / 1e9
   println(s"Time taken: $durationInSeconds seconds")
+
+  import scala.jdk.CollectionConverters._
+
+  val iterator = concurrentQueue.iterator().asScala
+  iterator.foreach(item => println(s"Item: $item"))
   
   bufferedSource.close() // Close the file reading
 }
