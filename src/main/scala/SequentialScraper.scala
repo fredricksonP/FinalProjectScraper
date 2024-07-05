86bec6c8a6d635278443a3f12c4641cdfab00164
Porter <peapodpuffer@gmail.com>
2024-07-05 21:44:32 +0700

    Focusing on refactoring and cleaning tonight
    - I will comment all of my code and clean it up. I will need to present it soon and I don't want it to be a mess.

--- src/main/scala/SequentialScraper.scala
+++ src/main/scala/SequentialScraper.scala
@@ -4,12 +4,24 @@ import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 
+/*
+  This was the first file I made for this final project. The goal of this
+  file was to learn how to web scrape using jsoup and scala. In FunPar Assignment 3,
+  we learned how to establish connections to pages with jsoup and how to obtain some basic
+  information from a web page. However, I needed to learn how to scrape target data from a webpage.
+  To learn how to do this, I followed a helpful web scraping tutorial which taught me how to scrape
+  product information from a fake E-commerce page that's designed for scraping. This was an iterative
+  process where first I learned how to scrape one product from the page, and then I learned how to scrape
+  all of the products from the E-commerce page. This wast the scraping tutorial I followed.
+  https://www.zenrows.com/blog/scala-web-scraping#convert-data-to-csv
+ */
 
 
 object SequentialScraper extends App {
   //Defining a structure to hold the scraped data
   case class Product(name: String, url: String, image: String, price: String)
 
+  //Connect to the E-commerce page
   val browser = JsoupBrowser()
   val doc = browser.get("https://www.scrapingcourse.com/ecommerce/")
   val html = doc.toHtml
