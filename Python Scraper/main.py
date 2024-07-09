042dc71554f76a1e19cb05c17c65a947500431be
Porter <peapodpuffer@gmail.com>
2024-07-09 18:08:30 +0700

    Double session tonight. Finally scraping proxies

--- Python Scraper/main.py
+++ Python Scraper/main.py
@@ -3,14 +3,58 @@ from bs4 import BeautifulSoup as BS
 import concurrent.futures
 import time
 
+# This tutorial helped me get started with scraping in python
+# https://nanonets.com/blog/web-scraping-with-python-tutorial/
+
+
+##Working on rotating proxies this afternoon
+# proxy_website = "https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt"
+# r = requests.get(proxy_website)
+# soup = BS(r.content, 'html.parser')
+# # print(r.text)
+# print(soup.text)
+# URL of the webpage to scrape
+# URL of the page to scrape
+# URL of the raw text file containing the proxy list
+url = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt"
+response = requests.get(url) #connect to github
+
+# if req is successful
+if response.status_code == 200:
+    proxy_list_text = response.text
+    # Split text into lines
+    proxy_list_lines = proxy_list_text.splitlines()
+    
+    # print proxies
+    for proxy in proxy_list_lines:
+        print(proxy)
+# print error message if connectoin fails
+else:   
+    print(f"Failed to retrieve the proxy list. Status code: {response.status_code}")
+
+# if r.status_code == 200:
+#     # Parse the HTML content using BeautifulSoup
+#     soup = BS(r.content, 'html.parser')
+    
+#     # Find the textarea element by its id
+#     textarea = soup.find('textarea', id='read-only-cursor-text-area')
+    
+#     # Extract the text content from the textarea
+#     if textarea:
+#         data = textarea.get_text()
+#         print("Scraped data:\n", data)
+#     else:
+#         print("Textarea element not found.")
+
+
 ##Testing rotating proxies 
-proxy = '116.100.233.88:1007'
-r = requests.get('https://httpbin.org/ip', proxies={'http': proxy, 'https': proxy}, timeout=3)
+# proxy = '116.100.233.88:1007'
+# r = requests.get('https://httpbin.org/ip', proxies={'http': proxy, 'https': proxy}, timeout=3)
 # r = requests.get('https://www.amazon.com/s?k=The+Coming+Woman%3A+A+Novel+Based+on+the+Life+of+the+Infamous+Feminist%2C+Victoria+Woodhull', proxies={'http': proxy, 'https': proxy}, timeout=3)
 # content = BS(r.content, 'html.parser')
 # bookTitle = content.find('span').text
 # print("\n" + bookTitle)
-print(r.status_code)
+# print(r.status_code)
 
 # print(r.json())
 
