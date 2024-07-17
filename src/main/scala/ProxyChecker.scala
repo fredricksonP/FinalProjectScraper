42a2878d08a933319edd01a5814f8f376c74fafd
Porter <peapodpuffer@gmail.com>
2024-07-17 23:08:39 +0700

    Can't get proxies writing out to file

--- src/main/scala/ProxyChecker.scala
+++ src/main/scala/ProxyChecker.scala
@@ -4,7 +4,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
 import java.io.{File, PrintWriter}
 import org.jsoup.Jsoup
-import org.jsoup.Connection
 import scala.util.{Failure, Success}
 
 object ProxyChecker extends App {
@@ -14,43 +13,58 @@ object ProxyChecker extends App {
   val potentialProxiesFile = "potential_proxies.txt"
 
   def checkProxies(proxy: String): Unit = {
-    println(s"Checking $proxy...")
-    try {
+    println(s"Checking $proxy")
+    val proxyHost = proxy.split(":")(0)
+    val proxyPort = proxy.split(":")(1).toInt
+    val timeout = 3.seconds  // Manual timeout for the entire proxy check process
+
+    // Create a Future to handle the proxy check
+    val proxyCheckFuture = Future {
       val response = Jsoup.connect("http://books.toscrape.com/catalogue/category/books_1/index.html")
-        .proxy(proxy.split(":")(0), proxy.split(":")(1).toInt)
-        .timeout(3000)
+        .proxy(proxyHost, proxyPort)
+        .timeout(3000)  // Timeout for the connection itself
         .execute()
 
-      if (response.statusCode == 200) {
-        validProxies.add(proxy)
-        val pw = new PrintWriter(new java.io.FileOutputStream(new File(workingProxiesFile), true))
-        pw.println(proxy)
-        pw.close()
-        println(s"SUCCESS: $proxy")
-      } else {
-        println(s"$proxy Failed with status ${response.statusCode}")
+      response.statusCode() match {
+        case 200 =>
+          println(s"SUCCESS: $proxy")
+          validProxies.add(proxy)
+          val pw = new PrintWriter(new java.io.FileOutputStream(new File(workingProxiesFile), true))
+          pw.println(proxy)
+          pw.close()
+          println(s"SUCCESS: $proxy")
+        case _ => println(s"$proxy Failed with status ${response.statusCode()}")
+      }
+    }
+
+    // Apply a timeout to the Future
+    try {
+      Await.ready(proxyCheckFuture, timeout).value.get match {
+        case Success(_) => // Proxy check succeeded
+        case Failure(e) => println(s"$proxy Failed with exception ${e.getMessage}")
       }
     } catch {
-      case e: Exception => println(s"$proxy Failed with exception ${e.getMessage}")
+      case e: TimeoutException => println(s"$proxy Timeout after $timeout")
     }
   }
 
   def fetchProxies(apiUrl: String): Option[String] = {
     try {
       val response = Jsoup.connect(apiUrl).ignoreContentType(true).execute()
-      if (response.statusCode == 200) Some(response.body) else None
+      if (response.statusCode() == 200) Some(response.body()) else None
     } catch {
       case e: Exception => println(s"Failed to fetch proxies: ${e.getMessage}"); None
     }
   }
 
+  // Get the latest proxies from proxy scrape and write them to a file
   fetchProxies(apiUrl).foreach { fileText =>
     val pw = new PrintWriter(potentialProxiesFile)
     pw.write(fileText)
     pw.close()
   }
 
-  val proxies = Source.fromFile("potential_proxies.txt").getLines().toList
+  val proxies = Source.fromFile(potentialProxiesFile).getLines().toList
   val futures = proxies.map(proxy => Future { checkProxies(proxy) })
 
   val aggregated = Future.sequence(futures)
